bf62b8b915121401734e7a3e28458e97548c08fc
Jonny Sweeny <jsweeny@gmail.com>
2012-11-03 14:11:28 -0400

    Rails generated app with thin

---
+++ course_feedback/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseFeedback::Application.config.secret_token = 'e7c729d6e247aca3d11047169ce9d6493c71ff7161c11b79f99d9722343bc099d4fc647dd59c7995919e8a950c9d71e88de5fb0d39bcea41e34fedc9574ccc91'
